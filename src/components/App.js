97693736ba40598050fee5e27db755b96fefe40c
Erick Kurniawan <erick.kurniawan@gmail.com>
2019-08-24 10:25:50 +0700

    menambahkan browser router

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,29 @@
-import React from 'react';
-import {BrowserRouter,Route} from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
+const PageOne = () => {
+  return <div>PageOne</div>;
+};
+
+const PageTwo = () => {
+  return (
+    <div>
+      <button>Click Me !</button>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={PageOne} />
+          <Route path="/pagetwo" component={PageTwo} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
