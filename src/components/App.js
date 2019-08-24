2b1e8e4662de7704ba1fec3650ce1bf3f499439a
Erick Kurniawan <erick.kurniawan@gmail.com>
2019-08-24 10:54:37 +0700

    menambahkan stream route

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,21 @@
 import React from "react";
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 import { BrowserRouter, Route } from "react-router-dom";
-
-const PageOne = () => {
-  return <div>PageOne</div>;
-};
-
-const PageTwo = () => {
-  return (
-    <div>
-      <button>Click Me !</button>
-    </div>
-  );
-};
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={PageOne} />
-          <Route path="/pagetwo" component={PageTwo} />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" component={StreamCreate} />
+          <Route path="/streams/edit" component={StreamEdit} />
+          <Route path="/streams/delete" component={StreamDelete} />
+          <Route path="/streams/show" component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
